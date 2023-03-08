e33ccd9ecf5e9fd8bd7c7bae375763767c177606
Shane Arland <arlandshane@gmail.com>
2023-03-08 11:07:34 +0530

    sixth commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const port = process.env.Port || 3000
+const port = process.env.PORT || 3000
 
 const app = express()
 
@@ -10,15 +10,15 @@ app.get('/', (req, res) => {
 })
 
 app.get('/hobbies', (req, res) => {
-    res.sendFile(__dirname + '/hobbies.html')
+    res.sendFile(__dirname + '/public/hobbies.html')
 })
 
 app.get('/contact', (req, res) => {
-    res.sendFile(__dirname + '/contact.html')
+    res.sendFile(__dirname + '/public/contact.html')
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/about.html')
+    res.sendFile(__dirname + '/public/about.html')
 })
 
 app.listen(port, (req, res) => {
