1a0e2f64abfd9f8b668979040cc8ecd73ad4d60e
Shane Arland <arlandshane@gmail.com>
2023-03-08 11:10:55 +0530

    seventh commit

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-const express = require('express')
-const port = process.env.PORT || 3000
+const express = require('express');
+const port = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/index.html');
 })
 
 app.get('/hobbies', (req, res) => {
-    res.sendFile(__dirname + '/public/hobbies.html')
+    res.sendFile(__dirname + '/public/hobbies.html');
 })
 
 app.get('/contact', (req, res) => {
-    res.sendFile(__dirname + '/public/contact.html')
+    res.sendFile(__dirname + '/public/contact.html');
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/public/about.html')
+    res.sendFile(__dirname + '/public/about.html');
 })
 
 app.listen(port, (req, res) => {
     console.log("Server up and running on port " + port);
 })
+
+module.exports = app;
